8e1646c62448cad6c357c470fa1a34c1aa098ff3
ZX <2327theforce@gmail.com>
2018-10-05 22:50:43 +0800

    package引用测试

--- src/PackageTest.java
+++ src/PackageTest.java
@@ -1,3 +1,18 @@
+import com.sun.xml.internal.messaging.saaj.util.CharReader;
+import com.horstmann.corejava.*;
+import static java.lang.System.*;
+
 public class PackageTest
 {
+    public static void main(String[] args)
+    {
+        //because of the import statement, we don't have to use
+        //com.horstmann.corejava.Employee here
+        Employee Chandler = new Employee("Chandler", 20000, 1999, 10, 1);
+
+        Chandler.raiseSalary(5);
+
+        //because of the static import statement, we don't have to use System.out here
+        out.println("name=" + Chandler.getName() + ", salary=" + Chandler.getSalary())
+    }
 }
