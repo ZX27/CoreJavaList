561bcf3652024489d22691abb6806c44872a537a
ZX <2327theforce@gmail.com>
2018-10-06 10:37:08 +0800

    First hello world

---
+++ src/OneToFourChapter/FirstSample.java
@@ -0,0 +1,9 @@
+package OneToFourChapter;
+
+public class FirstSample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the real world! It sucks. You are gonna love it");
+    }
+}
