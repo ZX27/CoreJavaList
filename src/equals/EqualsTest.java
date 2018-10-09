9318533b46754bab5a87ae7bae445574029aec46
ZX <2327theforce@gmail.com>
2018-10-09 12:49:27 +0800

    Equal method test

--- src/equals/EqualsTest.java
+++ src/equals/EqualsTest.java
@@ -16,7 +16,7 @@ public class EqualsTest
         System.out.println("chandler1 == chandler2: " + (chandler1 == chandler2));
         System.out.println("chandler1 == chandler3: " + (chandler1 == chandler3)); //不指向同一个对象，返回false
         System.out.println("chandler1.equals(chandler3): " + chandler1.equals(chandler3));//值相等，equals返回true
-        System.out.println("chandler.equals(ross): " + chandler1.equals(ross));
+        System.out.println("chandler1.equals(ross): " + chandler1.equals(ross));
         System.out.println("ross.toString(): " + ross);
 
         Manager monica = new Manager("Monica Geller", 80000, 1998, 5, 18);
@@ -24,7 +24,7 @@ public class EqualsTest
         boss.setBonus(5000);
 
         System.out.println("boss.toString(): " + boss);
-        System.out.println("monica.equals(boss): " + monica.equals(boss));
+        System.out.println("monica.equals(boss): " + monica.equals(boss));//boss对象的bonus值为5000，和monica不同，返回false
         System.out.println("chandler1.hashCode(): " + chandler1.hashCode());
         System.out.println("chandler3.hashCode(): " + chandler3.hashCode());
         System.out.println("ross.hashCode(): " + ross.hashCode());
