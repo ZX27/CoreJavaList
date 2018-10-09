72bf5ac005c08f97419b8627bf96c22d2c6e8012
ZX <2327theforce@gmail.com>
2018-10-09 10:56:02 +0800

    Equal method test

---
+++ src/equals/EqualsTest.java
@@ -0,0 +1,35 @@
+package equals;
+
+import java.awt.*;
+
+/**This program demonstrates the equals method.
+ * @author ZX27
+ */
+
+public class EqualsTest
+{
+    public static void main(String[] args)
+    {
+        Employee chandler1 = new Employee("Chandler Bing", 50000, 1999, 12, 21);
+        Employee chandler2 = chandler1;
+        Employee chandler3 = new Employee("Chandler Bing", 50000, 1999, 12, 21);
+        Employee ross = new Employee("Ross Geller", 40000, 1998, 10, 20);
+
+        System.out.println("chandler1 == chandler2: " + (chandler1 ==chandler2));
+        System.out.println("chandler1 == chandler3: " + (chandler1 == chandler3));
+        System.out.println("chandler1.equals(chandler3): " + chandler1.equals(chandler3));
+        System.out.println("chandler.equals(ross): " + chandler1.equals(ross));
+        System.out.println("ross.toString(): " + ross);
+
+        Manager monica = new Manager("Monica Geller", 80000, 1998, 5, 18);
+        Manager boss = new Manager("Monica Geller", 80000, 1998, 5, 18);
+        boss.setBonus(5000);
+
+        System.out.println("boss.toString(): " + boss);
+        System.out.println("monica.equals(boss): " + monica.equals(boss));
+        System.out.println("chandler1.hashCode(): " + chandler1.hashCode());
+        System.out.println("chandler3.hashCode(): " + chandler3.hashCode());
+        System.out.println("ross.hashCode(): " + ross.hashCode());
+        System.out.println("monica.hashCode(): " + monica.hashCode());
+    }
+}
