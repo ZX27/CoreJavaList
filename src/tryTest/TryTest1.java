8fec503ec0212efa402d887149ae69869c45cbd2
ZX <2327theforce@gmail.com>
2018-11-14 09:02:41 +0800

    异常捕获的示例代码

---
+++ src/tryTest/TryTest1.java
@@ -0,0 +1,35 @@
+package tryTest;
+
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
+
+import java.net.CacheRequest;
+
+public class TryTest1
+{
+    public TryTest1()
+    {
+        try
+        {
+            int a[] = new int[2];
+            a[4] = 3;
+            System.out.println("在异常处理后，会返回到这里吗？");
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            System.out.println("Exception message: " + e.getMessage());
+            System.out.println("Exception string: " + e.toString());
+            e.printStackTrace();
+        }
+        finally
+        {
+            System.out.println("--------------");
+            System.out.println("FINALLY");
+        }
+        System.out.println("No exception?");
+    }
+
+    public static void main(String[] args)
+    {
+        new TryTest1();
+    }
+}
---
+++ src/tryTest/TryTest2.java
@@ -0,0 +1,59 @@
+package tryTest;
+
+public class TryTest2
+{
+    public TryTest2()
+    {
+        for (int i=0; i<2; i++)
+        {
+            int k;
+            try
+            {
+                System.out.println("\nOuter try block; Test Case #" + i);
+                try
+                {
+                    System.out.println("\tInner try block");
+                    switch (i)
+                    {
+                        case 0:
+                            int zero = 0;
+                            k = 8/zero;
+                            break;
+                        case 1:
+                            int[] c = new int[3];
+                            k = c[5];
+                            break;
+                    }
+                }
+                catch (ArithmeticException e)
+                {
+                    System.out.println("\tInner> " + e);
+                }
+                catch (IndexOutOfBoundsException e)
+                {
+                    System.out.println("\tInner> " + e);
+                    //ArrayIndexOutOfBoundsException是IndexOutOfBoundsException的子类
+                    //所以内层捕获父类后为了保证外层捕获子类，再次抛出异常
+                    throw e;
+                }
+                finally
+                {
+                    System.out.println("\tInner finally block");
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException e)
+            {
+                System.out.println("\tOuter> " + e);
+            }
+            finally
+            {
+                System.out.println("Outer finally block");
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new TryTest2();
+    }
+}
