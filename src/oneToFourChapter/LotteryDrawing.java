849ab72f23da970b407523657390124886d06b52
ZX27 <43877472+ZX27@users.noreply.github.com>
2021-03-01 20:46:29 +0800

    错误修正，注释补全

--- src/oneToFourChapter/LotteryDrawing.java
+++ src/oneToFourChapter/LotteryDrawing.java
@@ -13,39 +13,31 @@ public class LotteryDrawing {
         System.out.print("What is the highest number you can draw?");
         int n = in.nextInt();
 
+        //fill an array with number 1 2 3 ... n
         int[] numbers = new int[n];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
         }
 
+        //draw k numbers and put them into a second array
         int[] results = new int[k];
         for (int i = 0; i < results.length; i++) {
+
+            //make a random index between 0 and n-1
             int r = (int) (Math.random() * n);
-            results[i] = numbers[r];
+
+            //pick the element at the random location
+            results[i] = numbers[r]; //抽取的是numbers数组的下标
+
+            //move the last element int the random location
             numbers[r] = numbers[n - 1];//不重复取数字
             n--;
         }
 
+        //print the sorted array
         Arrays.sort(results);
-        System.out.println("Bet tje following combination. It'll make you rich!");
+        System.out.println("Bet the following combination. It'll make you rich!");
         for (int i : results)
             System.out.println(i);
-    }
-
-    public static class InputTest {
-        public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-
-            //get first input
-            System.out.print("What's your name?");
-            String name = in.nextLine();
-
-            //get second input
-            System.out.print("How old are you?");
-            int age = in.nextInt();
-
-            //display output on console
-            System.out.println("Hello, " + name + ". Next Year, You'll be " + (age + 1));
-        }
     }
 }
