0d9a6215480d9df6a1712dabfe1a541f02e74998
ZX27 <43877472+ZX27@users.noreply.github.com>
2021-03-01 18:06:21 +0800

    格式化代码

--- src/oneToFourChapter/EmployeeTest.java
+++ src/oneToFourChapter/EmployeeTest.java
@@ -2,58 +2,48 @@ package oneToFourChapter;
 
 import java.time.LocalDate;
 
-public class EmployeeTest
-{
-    public static void main(String[] args)
-    {
+public class EmployeeTest {
+    public static void main(String[] args) {
         Employee[] staff = new Employee[3];
 
         staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 5);
         staff[1] = new Employee("Luke Skywalker", 100000, 1988, 8, 21);
         staff[2] = new Employee("Rachel Green", 12034, 1998, 5, 23);
 
-        for (Employee e : staff)
-        {
+        for (Employee e : staff) {
             e.raiseSalary(5);
         }
 
-        for (Employee e : staff)
-        {
+        for (Employee e : staff) {
             System.out.println("name=" + e.getName() + ", salary=" + e.getSalary() + ", hireDay=" + e.getHireDay());
         }
 
     }
 
-     static class Employee
-    {
+    static class Employee {
         private String name;
         private double salary;
         private LocalDate hireDay;
 
-        public Employee(String n, double s, int year, int month, int day)
-        {
+        public Employee(String n, double s, int year, int month, int day) {
             name = n;
             salary = s;
             hireDay = LocalDate.of(year, month, day);
         }
 
-        public String getName()
-        {
+        public String getName() {
             return name;
         }
 
-        public double getSalary()
-        {
+        public double getSalary() {
             return salary;
         }
 
-        public LocalDate getHireDay()
-        {
+        public LocalDate getHireDay() {
             return hireDay;
         }
 
-        public void raiseSalary(double byPercent)
-        {
+        public void raiseSalary(double byPercent) {
             double raise = salary * byPercent / 100;
             salary += raise;
         }
