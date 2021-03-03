2265e9c4fc9cbcb4df710cfd792aa1d4e8153383
ZX27 <43877472+ZX27@users.noreply.github.com>
2021-03-03 10:40:30 +0800

    修改注释

--- src/oneToFourChapter/LotteryArray.java
+++ src/oneToFourChapter/LotteryArray.java
@@ -13,6 +13,8 @@ public class LotteryArray {
         //fill triangular array
         for (int n = 0; n < odds.length; n++) {
             for (int k = 0; k < odds[n].length; k++) {
+
+                //compute binomial coefficient n*(n-1)*(n-2)*...*(n-k+1)/(1*2*3*...*k)
                 int lotteryOdds = 1;
                 for (int i = 1; i <= k; i++) {
                     lotteryOdds = lotteryOdds * (n - i + 1) / i;
